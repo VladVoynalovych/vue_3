dd4fe9fe171154e4ea80368ed9269613f4eb91ac
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2023-06-25 23:44:16 +0300

    update(UserPage): table with user data

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,5 +1,37 @@
 <template>
-  <article class="page"></article>
+  <article class="page">
+    <v-table height="300px">
+      <tbody>
+        <tr>
+          <td>
+            <v-img
+              :width="31"
+              aspect-ratio="16/9"
+              cover
+              :src="user?.avatar ?? ''"
+            />
+          </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Name:</td>
+          <td>{{ user?.name }}</td>
+        </tr>
+        <tr>
+          <td>Email:</td>
+          <td>{{ user?.email }}</td>
+        </tr>
+        <tr>
+          <td>Department:</td>
+          <td>{{ user?.department }}</td>
+        </tr>
+        <tr v-if="head">
+          <td>Head:</td>
+          <td>{{ head?.name }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </article>
 </template>
 <script setup lang="ts">
 import { useUsersStore } from "@/store/app";
@@ -9,7 +41,7 @@ const route = useRoute();
 
 const usersStore = useUsersStore();
 const user = usersStore.getUserById(route.params.id.toString());
-console.log("user >>>", user);
+const head = usersStore.getUserById(user?.head ?? "");
 </script>
 
 <script lang="ts">

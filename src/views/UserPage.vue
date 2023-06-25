ad1e2ae9aa4003d2240ba9c75a094925e4e27e1a
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2023-06-25 23:00:40 +0300

    add(UserPage): empty UserPage component

---
+++ src/views/UserPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <article class="page"></article>
+</template>
+<script setup lang="ts">
+import { useUsersStore } from "@/store/app";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const usersStore = useUsersStore();
+const user = usersStore.getUserById(route.params.id.toString());
+console.log("user >>>", user);
+</script>
+
+<script lang="ts">
+export default {
+  name: "UserPage",
+};
+</script>
+
+<style scoped></style>
